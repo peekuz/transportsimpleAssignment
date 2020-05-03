73e47fad60db2f39ed16428f5f7de77802b33c94
peekuz <ems.shafeeq@gmail.com>
2020-05-03 10:53:18 +0530

    except curve

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,15 +16,16 @@ export class AppComponent {
   toPoint = ""
   lastfromPoint = ""
   lasttoPoint = ""
-  x: number = 20;
-  y: number = 40;
+  x: number = 40;
+  y: number = 20;
   level2: boolean = false;
   prev_level2: boolean = false;
   discontinuos_travel: boolean = false;
   states: string[] = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware'];
   selectedFromplaces = [...this.states]
   selectedToplaces = [...this.states]
-
+  colors: string[] = ['green', 'blue', '#8C9904', '#575F02', '#69BB06', '#467F02', '#02507F', '#08027F', '#3B027F', '#2C025F', '#48025F', '#380146']
+  colorIndex: number = 0;
   @ViewChild('canvas')
   canvas: ElementRef<HTMLCanvasElement>;
 
@@ -52,14 +53,18 @@ export class AppComponent {
 
   addPlaceFrom(newPlace) {
     this.states.push(newPlace)
-    this.onKeyFrom("")
+    // this.onKeyFrom("")
+    this.selectedFromplaces = [...this.states]
+    this.selectedToplaces = [...this.states]
     this.searchValueFrom = ""
     this.newFromPlace = ""
 
   }
   addPlaceTo(newPlace) {
     this.states.push(newPlace)
-    this.onKeyTo("")
+    this.selectedToplaces = [...this.states]
+    this.selectedFromplaces = [...this.states]
+    // this.onKeyTo("")
     this.searchValueTo = ""
     this.newToPlace = ""
   }
@@ -70,13 +75,17 @@ export class AppComponent {
   }
 
   drawcircle() {
+    this.x = this.x + 5;
     this.ctx.beginPath();
     this.ctx.arc(this.x, this.y, 5, 0, 2 * Math.PI);
-    this.ctx.fillText(this.fromPoint.slice(0, 3) + "-" + this.toPoint.slice(0, 3), this.x - 20, this.y + 20);
+    this.ctx.fillText(this.fromPoint.slice(0, 3).toUpperCase() + "-" + this.toPoint.slice(0, 3).toUpperCase(), this.x - 20, this.y + 20);
+    this.ctx.fillStyle = this.colors[this.colorIndex];
+    this.ctx.strokeStyle = this.colors[this.colorIndex];
     this.ctx.stroke();
     this.x = this.x + 5;
   }
   drawline(x_len, y_len) {
+    this.x = this.x + 5;
     this.ctx.beginPath();
     this.ctx.moveTo(this.x, this.y);
     this.ctx.lineWidth = 2;
@@ -86,9 +95,12 @@ export class AppComponent {
     this.y = this.y + y_len;
     this.ctx.lineTo(this.x, this.y);
     this.x = this.x + 5;
+    this.ctx.fillStyle = this.colors[this.colorIndex];
+    this.ctx.strokeStyle = this.colors[this.colorIndex];
     this.ctx.stroke();
   }
   drawarrowline(x_len, y_len, ang) {
+    this.x = this.x + 5;
     this.ctx.beginPath();
     this.ctx.moveTo(this.x, this.y);
     this.ctx.lineWidth = 2;
@@ -98,6 +110,8 @@ export class AppComponent {
     this.y = this.y + y_len;
     this.ctx.lineTo(this.x, this.y);
     this.x = this.x + 5;
+    this.ctx.fillStyle = this.colors[this.colorIndex];
+    this.ctx.strokeStyle = this.colors[this.colorIndex];
     this.ctx.stroke();
 
     this.ctx.beginPath();
@@ -106,12 +120,16 @@ export class AppComponent {
     this.ctx.lineTo(this.x - 12, this.y + 5);
     this.ctx.closePath();
     this.ctx.fill();
+    this.ctx.fillStyle = this.colors[this.colorIndex];
+    this.ctx.strokeStyle = this.colors[this.colorIndex];
     this.x = this.x + 5;
 
 
 
   }
   addTrip() {
+    this.colorIndex = this.colorIndex + 1;
+    this.colorIndex = this.colorIndex % this.colors.length;
     if (this.lastfromPoint == "" && this.lasttoPoint == "") {
       this.drawcircle()
     }
@@ -120,9 +138,9 @@ export class AppComponent {
       //this.ctx.beginPath();
       if (!this.level2 && !this.prev_level2) {
         if (this.lastfromPoint != "" && this.lasttoPoint != "") {
-          this.ctx.clearRect(this.x - 90, this.y - 10, 95, 20)
+          this.ctx.clearRect(this.x - 98, this.y - 10, 100, 20)
           this.ctx.clearRect(this.x - 30, this.y, 60, 30)
-          this.x = this.x - 90;
+          this.x = this.x - 95;
         }
         if (!this.discontinuos_travel) {
           this.drawline(80, 80);
@@ -141,7 +159,7 @@ export class AppComponent {
         this.prev_level2 = true;
       }
       else if (!this.level2 && this.prev_level2) {
-        this.ctx.clearRect(this.x - 90, this.y - 10, 95, 90)
+        this.ctx.clearRect(this.x - 99, this.y - 10, 100, 20)
         this.ctx.clearRect(this.x - 30, this.y, 60, 30)
         this.x = this.x - 90;
         this.y = this.y + 80;
